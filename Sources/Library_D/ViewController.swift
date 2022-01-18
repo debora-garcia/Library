210a945e7760034fb9b167d7165b03e11162d641
debora-garcia <debora.garcia@g.globo>
2022-01-17 22:49:42 -0300

    Adicao de itens na pagina de login

--- Sources/Library_D/ViewController.swift
+++ Sources/Library_D/ViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 public class ViewController: UIViewController {
+    
+    @IBOutlet weak var getPassword: UITextField!
+    @IBOutlet weak var getEmail: UITextField!
     @IBOutlet weak var meuBotao: UIButton!
+    
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        
         meuBotao.setTitle("Fazer login", for: .normal)
+        
+        print("Email: \(getEmail), senha: \(getPassword)")
+         
     }
 }

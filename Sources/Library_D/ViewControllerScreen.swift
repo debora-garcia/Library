8d2520fbe439c094dad5ae93800e47085c62183b
Luiz Fernando Salvaterra <luizfernandosalvaterra@Luizs-MacBook-Air.local>
2022-01-20 09:17:45 -0300

    Adds new way to load xib files

--- Sources/Library_D/ViewControllerScreen.swift
+++ Sources/Library_D/ViewControllerScreen.swift
@@ -12,16 +12,15 @@ public class ViewControllerScreen: UIViewController {
     @IBOutlet weak var getPassword: UITextField!
     @IBOutlet weak var getEmail: UITextField!
     @IBOutlet weak var meuBotao: UIButton!
-        
-//        public init() {
-//        super.init(nibName: "ViewController", bundle: Bundle(for: ViewController.self))
-//            print("Teste Library")
-//        }
-//
-//        required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//        }
-    
+
+    override public init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+         super.init(nibName: "ViewControllerScreen", bundle: Bundle.module)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     public override func viewDidLoad() {
         super.viewDidLoad()
 //        meuBotao.tintColor = .black

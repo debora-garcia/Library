4e318fcfe619d01aef62c5e6b866bd6e10395faa
debora-garcia <debora.garcia@g.globo>
2022-01-31 17:17:00 -0300

    add action in button Entrar

--- Sources/Library_D/VIPER/ViewControllerScreen.swift
+++ Sources/Library_D/VIPER/ViewControllerScreen.swift
@@ -12,11 +12,17 @@ protocol LoginViewInterface: AnyObject {
 }
 
 public class ViewControllerScreen: UIViewController, LoginViewInterface {
-
+    
     var presenter: LoginModuleInterface?
+    
     @IBOutlet weak var getPassword: UITextField!
     @IBOutlet weak var getEmail: UITextField!
-    @IBOutlet weak var meuBotao: UIButton!
+    
+    @IBAction func loginAction(_ sender: Any) {
+        print("entrou na func loginAction")
+        self.presenter?.updateView()
+    }
+//    @IBOutlet weak var meuBotao: UIButton!
 
     override public init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
          super.init(nibName: "ViewControllerScreen", bundle: Bundle.module)
@@ -29,7 +35,8 @@ public class ViewControllerScreen: UIViewController, LoginViewInterface {
     public override func viewDidLoad() {
         super.viewDidLoad()
         print("Testando a view controller Screen")
-        self.presenter?.updateView()
+        self.presenter = LoginPresenter()
+//        self.presenter?.updateView()
         
 //        meuBotao.setTitle("Fazer login", for: .normal)
     }
